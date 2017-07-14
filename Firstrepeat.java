495da9b9da4a9957dd7617c53316264f421bce3a
DEEBIKA1305 <tdeepika4eee@gmail.com>
2017-07-14 14:46:53 +0530

    Add files via upload

---
+++ Firstrepeat.java
@@ -0,0 +1,35 @@
+package sololearn;
+import java.util.*;
+public class Onlyonce {
+public static void main(String args[]) {
+	
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the number of elemnts in an array:");
+	int n=s.nextInt();
+	int k=0;
+	int repeatarray[]=new int[n];
+	int array[]=new  int[n];
+	for(int i=0;i<n;i++) {
+		 array[i]=s.nextInt();
+	}
+	for(int i=0;i<n;i++) {
+		int count=0;
+		for(int j=i+1;j<n;j++) {
+			if(array[i]==array[j]) {
+				count++;
+				
+			}
+			if(count==1) {
+				repeatarray[k]=array[i];
+				k++;
+				break;
+			}
+			
+		}
+	}
+	int len=repeatarray.length;
+	System.out.println(repeatarray[0]);
+	
+	
+}
+}
